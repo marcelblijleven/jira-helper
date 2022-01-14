41fdf62969d9319d3308ad8e217f50382b192c6a
Marcel Blijleven <marcelblijleven@gmail.com>
2022-01-14 14:49:06 +0100

    chore: add flag names and usages as constant values

--- cmd/vars.go
+++ cmd/vars.go
@@ -7,4 +7,35 @@ var (
 	project string
 	version string
 	body    string
+	issues  []string
+)
+
+const (
+	userFlagName  = "user"
+	userShorthand = "u"
+	userUsage     = "User (email) for authenticating against the Jira API"
+
+	hostFlagName  = "host"
+	hostShorthand = "s"
+	hostUsage     = "Host of the Jira API. If the host URL contains a scheme (e.g. https), you must include it"
+
+	tokenFlagName  = "token"
+	tokenShorthand = "t"
+	tokenUsage     = "Token used to authenticate against the Jira API"
+
+	projectFlagName  = "project"
+	projectShorthand = "p"
+	projectUsage     = "Project key of the Jira project, e.g. MB"
+
+	versionFlagName  = "version"
+	versionShorthand = "v"
+	versionUsage     = "Name of the version"
+
+	bodyFlagName  = "releaseBody"
+	bodyShorthand = "b"
+	bodyUsage     = "The body of text which contains Jira issues, e.g. a GitHub release body"
+
+	issuesFlagName  = "issues"
+	issuesShorthand = "s"
+	issuesUsage     = "The issues you want to assign to release to, can be a single issue or comma separated"
 )
